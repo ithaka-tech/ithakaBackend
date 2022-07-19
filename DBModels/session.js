d17f4d3a1bae0f37897ea7decdfe83f5a9845b05
likhithb <lxb326@psu.edu>
2022-07-19 15:04:11 -0700

    deleting sessionId collections model

--- DBModels/session.js
+++ DBModels/session.js
@@ -1,10 +1,8 @@
 require('dotenv').config()
 
-const mongoose = require('mongoose');
 const Joi = require('joi');
 const jws = require('jws')
 
-const SessionSchema = mongoose.model('Sessions', mongoose.Schema({ }))
 
 function validateSessionBody(session){
     const pattern = /^[A-Za-z0-9]+.[A-Za-z0-9]+.[A-Za-z0-9]+$/
@@ -36,14 +34,5 @@ async function createNewSession(clientId, clientName, clientEmail){
     return jsonWebToken
 }
 
-
-async function validateSessionID(sessionID){
-    const exists = await SessionSchema.findById(sessionID).exec();
-    if (exists) return true; 
-    return false;
-}
-
-module.exports.SessionSchema = SessionSchema;
 module.exports.validateSessionBody = validateSessionBody;
-module.exports.validateSessionID = validateSessionID;
 module.exports.createNewSession = createNewSession;
--- Routes/sessions.js
+++ Routes/sessions.js
@@ -1,23 +1,7 @@
 const express = require('express');
-const { validateSessionID, validateSessionBody, SessionSchema } = require('../DBModels/session')
+const { validateSessionBody } = require('../DBModels/session')
 
 const router = express.Router();
 
-router.delete('/:sessionID', async (req, res) => {
-    //checking if the input is valid size
-    const { error } = validateSessionBody({ sessionID: req.params.sessionID })
-    if(error) return res.status(400).json(error.details[0].message)
-
-    //checking if the sessionID exists
-    const validSes = validateSessionID(req.params.sessionID)
-    if(!validSes) return res.status(404).send('Unauthorized Request')
-
-    //deleting the session from the table
-    const deletedSess = SessionSchema.deleteOne({ _id: req.params.sessionID });
-    return res.send({
-        deletedEntry: deletedSess
-    })
-
-})
 
 module.exports = router;
