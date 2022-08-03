47e59e45bd7c901ad233b3a018d5329f9cc3c7ff
likhithb <lxb326@psu.edu>
2022-08-03 12:56:37 -0700

    functioning email api for customer onboarding

--- Routes/emails.js
+++ Routes/emails.js
@@ -18,22 +18,48 @@ const sesv2 = new AWS.SESV2();
  * }
  */
 router.post('/signup', async (req, res) => {
+  
+  const clientName = 'Randy Brothers Landscaping'
+  const body = `<!DOCTYPE html>\
+  <html lang=\"en\">\
+    <head>\
+        <meta charset=\"UTF-8\"/>\
+        <title>Onboarding</title>\
+    </head>\
+    <body>\
+      <p>Hi! Thank you for your interest in <ins style=\"text-decoration: none;\">${clientName}</ins>. To ensure that we provide you \
+        with the best service possible, please fill out the following fields.\
+      </p>\
+      <a href=\"${process.env.CUSTOMER_ONBOARDING_DOMAIN}\"> Click to access form </a>\
+      <p>In the comments section, please note any special requests or items that we should be aware of when we visit your property. \
+        <br/>\
+        <br/>\
+        Jobs will be invoiced within a few days after they are completed and the invoice will be emailed to the email address provided to allow\
+        for a smooth and easy transaction.\
+        <br/>\
+        <br/>\
+        We take pride in the service we provide and always appreciate feedback, google reviews, or simply a smile! We look forward to working with you to make sure your \
+        home looks, and feels, like home.\
+      </p>\
+    </body>\
+  </html>`
+
   //Create sendEmail params 
   var params = {
     Destination: { 
       ToAddresses: [
-        'templetran23@gmail.com',
+        'likhithborela@gmail.com',
       ],
     },
     Message: { /* required */ 
       Body: { /* required */
         Html: {
         Charset: "UTF-8",
-        Data: "Temple, make me the template, or else i will spam you :)"
+        Data: body
         },
         Text: {
         Charset: "UTF-8",
-        Data: "idk",
+        Data: "",
         },
       },
       Subject: {
@@ -41,7 +67,7 @@ router.post('/signup', async (req, res) => {
         Data: 'Landscaping Job Request'
       }
       },
-    Source: 'mail@ithaka.tech', /* required */
+    Source: `${clientName.split(' ').join('')}@ithaka.tech`, /* required */
     Tags: [
       {
         Name: 'STRING', /* required */
@@ -64,6 +90,5 @@ router.post('/signup', async (req, res) => {
     });
 
 })
-
 
 module.exports = router
